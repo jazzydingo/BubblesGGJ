9c9443e42c1ebebe822fe10ca8cc959b42bd5947
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-01-24 09:23:22 -0700

    Dialogue
    got dialogue to work slight bug at end

--- Assets/GAME/Scripts/DialogueManager.cs
+++ Assets/GAME/Scripts/DialogueManager.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,7 +12,9 @@ public class DialogueManager : MonoBehaviour
     public string characterName;
     public string[] dialogue;
     public string currentLine;
-    private int index; 
+    public int index; 
+    public bool isTyping;
+    public bool end;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,25 +22,52 @@ public class DialogueManager : MonoBehaviour
         index = 0;
         nameTag.text = characterName;
         body.text = "";
-        currentLine = dialogue[index];
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         body.text = currentLine;
     }
-
-    void OnMouseDown()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        if(index < dialogue.Length)
+        if(InputSystem.actions["Move"].ReadValue<Vector2>().y == 1 && !isTyping)
+        {
+            StartCoroutine(TypeLine());
+        }
+        else if(InputSystem.actions["Move"].ReadValue<Vector2>().y == 1 && end)
+        {
+            index = 0;
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator TypeLine()
+    {
+        currentLine = "";
+        isTyping = true;
+        foreach(char c in dialogue[index])
+        {
+            currentLine += c;
+            yield return new WaitForSeconds(0.01f);
+        }
+        NextLine();
+        isTyping = false;
+        yield return null;
+    }
+
+    void NextLine()
+    {   
+        
+        if(index < dialogue.Length - 1)
         {
             index++;
         }
         else
         {
-            if(index == dialogue.Length)
-            this.gameObject.SetActive(false);
+            if(index == dialogue.Length - 1)
+            end = true;
         }
     }
 }
