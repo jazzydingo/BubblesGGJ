cfeed13567070db23a1180bbcd4df237da6da749
Gorkovoy1 <101889868+Gorkovoy1@users.noreply.github.com>
2025-01-26 14:07:33 -0700

    music changes

--- Assets/GAME/Scripts/MenuScripts/MainMenuButtons.cs
+++ Assets/GAME/Scripts/MenuScripts/MainMenuButtons.cs
@@ -1,10 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuButtons : MonoBehaviour
 {
+    private uint soundID;
+    public GameObject sfxObj;
+
+    public void Start()
+    {
+        sfxObj = GameObject.FindWithTag("SFX");
+    }
     public void PlayGame()
     {
+        //play sound
+        AkSoundEngine.PostEvent("play_octo_opening", sfxObj);
+        //wait until sound end
+        StartCoroutine(WaitForSound());
+
+
+    }
+
+    IEnumerator WaitForSound()
+    {
+        yield return new WaitForSeconds(5f); 
         SceneManager.LoadSceneAsync(1);
     }
 
