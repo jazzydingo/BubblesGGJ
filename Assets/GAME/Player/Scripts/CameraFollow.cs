fb7595c2a195ac22e7fe69be455f8770f999bdeb
jazzydingo <129543893+jazzydingo@users.noreply.github.com>
2025-01-26 11:48:00 -0700

    final scene bubble rise

--- Assets/GAME/Player/Scripts/CameraFollow.cs
+++ Assets/GAME/Player/Scripts/CameraFollow.cs
@@ -4,10 +4,23 @@ public class Follow_player : MonoBehaviour
 {
 
     public Transform player;
+    public bool finalLevel;
+    public GameObject bubble;
 
     // Update is called once per frame
+    void Start()
+    {
+
+    }
     void Update()
     {
+        bubble = GameObject.FindWithTag("Bubble");
         transform.position = player.transform.position + new Vector3(0, 1, -5);
+
+        if(finalLevel  && bubble != null)
+        {
+            transform.position = bubble.transform.position + new Vector3(0, 1, -5);
+        }
+        
     }
 }
