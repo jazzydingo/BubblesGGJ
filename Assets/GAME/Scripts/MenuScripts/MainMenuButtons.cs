d4414c93243b047570224f895b10fbda585b5d1a
Gorkovoy1 <101889868+Gorkovoy1@users.noreply.github.com>
2025-01-27 00:37:00 -0700

    menu music bug

--- Assets/GAME/Scripts/MenuScripts/MainMenuButtons.cs
+++ Assets/GAME/Scripts/MenuScripts/MainMenuButtons.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using AK.Wwise;
 
 public class MainMenuButtons : MonoBehaviour
 {
@@ -8,7 +9,32 @@ public class MainMenuButtons : MonoBehaviour
     public GameObject sfxObj;
     public GameObject MenuMusic;
 
-    public void Start()
+    void Awake()
+    {
+        StartCoroutine(LoadSoundBankAsync());
+    }
+
+    IEnumerator LoadSoundBankAsync()
+    {
+        string soundBankPath = Application.streamingAssetsPath + "/Audio/GeneratedSoundBanks/Main.bnk";
+        uint bankID;
+
+        // Load the soundbank asynchronously
+        AKRESULT result = AkSoundEngine.LoadBank(soundBankPath, out bankID);
+
+        if (result == AKRESULT.AK_Success)
+        {
+            Debug.Log("SoundBank loaded successfully!");
+        }
+        else
+        {
+            Debug.LogError("Failed to load SoundBank.");
+        }
+
+        yield return null;
+    }
+
+public void Start()
     {
         sfxObj = GameObject.FindWithTag("SFX");
     }
